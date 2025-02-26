a3f0cb85b7fe13206069ff61de64c108c6caadfd
Bruno Kemel <br.kemel@gmail.com>
2025-02-26 07:44:16 -0300

    addd faqs e js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
     
         for (let i = 0; i< buttons.length; i++) {
             buttons[i].addEventListener('click', function(botao) {
@@ -12,6 +13,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
 
 function removeBotaoAtivo () {
@@ -22,6 +27,13 @@ function removeBotaoAtivo () {
     }
 }
 
+
+function abreOuFechaResposta (elemento) {
+   const classe = 'faq__questions__item--is--open';
+   const elementoPai = elemento.target.parentNode;
+
+   elementoPai.classList.toggle(classe);
+}
 
 function escondeTodasAbas () {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
